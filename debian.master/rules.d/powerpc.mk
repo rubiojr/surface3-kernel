33bf6534abfdf64d39a7ce7a8c104c19ce3acd4c
Ben Collins <bcollins@ubuntu.com>
2014-04-29 19:01:04 -0500

    UBUNTU: [Config] Re-enable powerpc64-emb
    Signed-off-by: Ben Collins <ben.c@servergy.com>
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- debian.master/rules.d/powerpc.mk
+++ debian.master/rules.d/powerpc.mk
@@ -2,8 +2,7 @@ human_arch	= PowerPC (32 bit userspace)
 build_arch	= powerpc
 header_arch	= $(build_arch)
 defconfig	= pmac32_defconfig
-flavours	= powerpc-smp powerpc64-smp powerpc-e500mc
-#powerpc64-emb
+flavours	= powerpc-smp powerpc64-smp powerpc-e500mc powerpc64-emb
 build_image	= zImage
 kernel_file	= $(shell if [ ! -f $(builddir)/build-$*/vmlinux.strip ] && \
 		    [ -f $(builddir)/build-$*/vmlinux.strip.gz ]; then \
@@ -15,6 +14,9 @@ install_file	= vmlinux
 # These flavours differ
 build_image_powerpc-e500mc	= uImage
 kernel_file_powerpc-e500mc	= arch/powerpc/boot/uImage
+
+build_image_powerpc64-emb	= uImage
+kernel_file_powerpc64-emb	= arch/powerpc/boot/uImage
 
 loader		= yaboot
 
